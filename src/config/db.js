7a062f06dab9b511a913f306a8460e96a486875c
sauravnalawade2004 <sauravnalawade143@gmail.com>
2026-01-30 13:12:30 -0800

    env

--- src/config/db.js
+++ src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 export const connectDatabase = async () => {
     try {
-        await mongoose.connect('127.00.0' || process.env.DB)
+        await mongoose.connect('mongodb+srv://sauravnalawade143_db_user:lBM7uOfOcPHWVRc3@airesumecluster0.hlytveu.mongodb.net/?appName=AiResumeCluster0' || process.env.DB)
         console.log("Database is connected")
     } catch (error) {
         console.error("Database is not connected")
