2d675cf756722b9107c034c8795d349ee511fe0c
sauravnalawade2004 <sauravnalawade143@gmail.com>
2026-02-14 21:42:19 -0800

    ai model setup

--- src/controllers/openaicontroller.js
+++ src/controllers/openaicontroller.js
@@ -27,7 +27,7 @@ const openAi = async (req, res) => {
 //controller for wnchancing a resume's professional summaery 
 //POST : app/ai/enhance-pro-sum
 
-const EnhanceProSummary = async (req, res) => {
+export const EnhanceProSummary = async (req, res) => {
      try {
         const {usercontent} = req.body;
 
@@ -38,18 +38,25 @@ const EnhanceProSummary = async (req, res) => {
             })
         }
 
-        await ai.chat.completions.create({
+       const response =  await ai.chat.completions.create({
             model: "gemini-2.0-flash",
             messages: [
-                { role: "system", content: "you are a helpful assistant"},
+                { role: "system", content: "you are a helpful assistant in resume writing, Your task is to enhance the professional summary of a resume. The summary should be 1-2 sentences also highlighting key skills, experience, and career objectives. make it compelling and ATS_friendly. and only return text no options or anything else"},
                 {
                     role: "user",
-                    content: "Explain to me how Ai works"
+                    content: usercontent
                 }
             ],
         })
+        const enhancedSummary = response.choices[0].message.content;
+        return res.status(200).json({
+            success: true,
+            message: "Enhanced Professional Summary",
+            data: response.choices[0].message.content
+        })
      } catch (error) {
         res.status(400).json({
+            enhancedSummary : null,
             message: "error in summary",
             success : "False"
         })
